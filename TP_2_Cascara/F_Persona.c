b9ad6ce1a934658c697a6ee79714ca9d31a5fc2b
elianaCvargas <carla.vargas.e@gmail.com>
2017-04-27 22:18:45 -0300

    Add files via upload

---
+++ TP_2_Cascara/F_Persona.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funciones.h"
+
+int initPersona(S_Persona* persona, int length)
+{
+    int retorno = 0;
+    int i;
+    for (i = 0; i<length ; i++)
+    {
+        persona[i].flag = 0;
+    }
+ return retorno;
+}
+
+int addPersona(S_Persona* persona, int length, int dni, char name[],int edad)
+{
+    int retorno = -1, i;
+
+    for (i = 0; i<length ; i++)
+    {
+        if (persona[i].flag == 0)
+        {
+            strcpy(persona[i].nombre, name);
+            persona[i].dni = dni;
+            persona[i].edad = edad;
+            persona[i].flag = 1;
+            retorno = 0;
+            break;
+        }
+    }
+ return retorno;
+}
+
+/*void cargarPersona(S_Persona personas[], int index, int id)
+{
+    printf("nombre director");
+    scanf("%s", directores[index].nombre);
+
+    printf("pais director");
+    scanf("%s", directores[index].paisOrigen);
+
+    printf("fecha nacimiento director");
+    scanf("%s", directores[index].fechaNac);
+
+    directores[index].flagFree=0;
+    directores[index].id = id;
+}*/
---
+++ TP_2_Cascara/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+#define ELEMENTS 5
+
+int main()
+{
+
+     S_Persona arrayPersona[ELEMENTS];
+    char seguir='s';
+    int opcion=0,  i, emptyStrutc, alta;
+
+
+
+
+
+    emptyStrutc = initPersona(arrayPersona, ELEMENTS);
+    /*for (i = 0; i<ELEMENTS ; i++)
+    {
+        printf("%d\n", arrayPersona[i].flag);
+    }*/
+    alta = addPersona(arrayPersona, ELEMENTS, 12345678,"Eliana", 28);
+    for (i = 0; i<ELEMENTS ; i++)
+    {
+
+        printf("%s\n %d\n %d\ \n", arrayPersona[i].nombre,arrayPersona[i].dni, arrayPersona[i].edad);
+        break;
+
+    }
+
+    while(seguir=='s')
+    {
+        printf("1- Agregar persona\n");
+        printf("2- Borrar persona\n");
+        printf("3- Imprimir lista ordenada por  nombre\n");
+        printf("4- Imprimir grafico de edades\n\n");
+        printf("5- Salir\n");
+
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                seguir = 'n';
+                break;
+        }
+    }
+
+    return 0;
+}
